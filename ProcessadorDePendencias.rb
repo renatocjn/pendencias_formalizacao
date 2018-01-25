ef1e4594de33dbc4043152f997eaa015dbb750e2
Renato Caminha Juacaba Neto <renatocjn@gmail.com>
2018-01-25 16:52:03 -0300

    closer than ever
    Still having problems with the main window not responding and some banks and proposals not working

--- ProcessadorDePendencias.rb
+++ ProcessadorDePendencias.rb
@@ -2,117 +2,114 @@ class String
   def is_integer?
     self =~ /^[-+]?[1-9]([0-9]*)?$/
   end
+
+  def remover_acentuacao
+    self.tr( "ÀÁÂÃÄÅàáâãäåĀāĂăĄąÇçĆćĈĉĊċČčÐðĎďĐđÈÉÊËèéêëĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħÌÍÎÏìíîïĨĩĪīĬĭĮįİıĴĵĶķĸĹĺĻļĽľĿŀŁłÑñŃńŅņŇňŉŊŋÒÓÔÕÖØòóôõöøŌōŎŏŐőŔŕŖŗŘřŚśŜŝŞşŠšſŢţŤťŦŧÙÚÛÜùúûüŨũŪūŬŭŮůŰűŲųŴŵÝýÿŶŷŸŹźŻżŽž",
+             "AAAAAAaaaaaaAaAaAaCcCcCcCcCcDdDdDdEEEEeeeeEeEeEeEeEeGgGgGgGgHhHhIIIIiiiiIiIiIiIiIiJjKkkLlLlLlLlLlNnNnNnNnnNnOOOOOOooooooOoOoOoRrRrRrSsSsSsSssTtTtTtUUUUuuuuUuUuUuUuUuUuWwYyyYyYZzZzZz")
+  end
 end
 
 module ProcessadorDePendencias
-	require 'tiny_tds'
-
-	BANK_TO_SPREADSHEET_COLUMN_NUMBER_OF_PROPOSALS = {
-		"teste" => 'E',
-		"help" => 'E',
-		"ole" => 'H',
-		"itau" => 'I'
-	}
-	
-	def getSpreadSheetColumn filename, bank
-		bank_keys = BANK_TO_SPREADSHEET_COLUMN_NUMBER_OF_PROPOSALS.keys
-		bank.downcase!
-		if bank_keys.include? bank
-			return BANK_TO_SPREADSHEET_COLUMN_NUMBER_OF_PROPOSALS[bank]
-		else
-			file_basename = File.basename(filename).downcase
-			bank_keys.each do |k|
-				if file_basename.include? k 
-					return BANK_TO_SPREADSHEET_COLUMN_NUMBER_OF_PROPOSALS[k]
-				end
-			end
-		end
-		raise "O banco desta planilha não pôde ser encontrado"
-	end
-	
-	def acquireListOfProposals file, bank
-		if file.end_with?("xls")
-			require "roo-xls"
-		else
-			require "roo"
-		end
-		
-		spreadsheet =  Roo::Spreadsheet.open(file)
-		column_number = getSpreadSheetColumn file, bank
-		#puts "Column #{column_number}"
-		
-		spreadsheet.column(column_number).select {|v| v.is_a?(Integer) or v.is_a?(Float) or v.is_integer?}
-	end
-	
-	def createDatabaseConnection
-		#puts "createDatabaseConnection"
-		
 		database_url = ""
-		#puts "database_url = '#{database_url}'"
-		
 		database_name = ""
-		#puts "database_name = '#{database_name}'"
-		
 		user_login = ""
-		#puts "user_login = '#{user_login}'"
-		
 		user_passwd = ""
-		#puts "user_passwd = '#{user_passwd}'"
-		
-		begin
-			TinyTds::Client.new username: user_login, password: user_passwd, host: database_url, database: database_name
-		rescue TinyTds::Error => err
-			raise "falha ao se conectar ao banco"
-		end 
-	end
-	
-	def getSQL proposal
-		"SELECT UE.SGL_UNIDADE_EMPRESA, UE.SGL_UNIDADE_FEDERACAO, UE.NOM_UNIDADE_EMPRESA, UE.NOM_FANTASIA
-			FROM [CBDATA].[dbo].[PROPOSTA_EMPRESTIMO] AS PE
-				INNER JOIN [CBDATA].[dbo].[UNIDADE_EMPRESA] AS UE ON UE.COD_UNIDADE_EMPRESA = PE.COD_UNIDADE_EMPRESA
-			WHERE PE.NUM_PROPOSTA = '#{proposal}' OR PE.NUM_CONTRATO = '#{proposal}'"
-	end
-	
-	def queryDatabaseForUfOfProposal con, proposal
-		sql = getSQL proposal
-		result = con.execute sql
-		row = result.first
-		if row.nil?
-			puts proposal
-			nil
-		else
-			uf = row["SGL_UNIDADE_EMPRESA"]
-			uf = uf.nil? ? row["SGL_UNIDADE_FEDERACAO"] : uf
-			uf = uf.nil? ? row["NOM_UNIDADE_EMPRESA"] : uf
-			uf = uf.nil? ? row["NOM_FANTASIA"] : uf
-		end
-	end
-	
-	def findUfOfEachProposal(proposals, progress_keeper=nil)
-		con = createDatabaseConnection
-		raise "Não foi possível acessar o banco de dados" unless con
-		failed_proposals = Array.new
-		response = proposals.collect do |proposal_number|
-			uf = queryDatabaseForUfOfProposal con, proposal_number
-			failed_proposals << proposal_number unless uf
-			progress_keeper.progress += 1 unless progress_keeper.nil?
-			[proposal_number, uf]
-		end
-		con.close
-		return response, failed_proposals
-	end
-	
-	def recoverProposalNumbersAndStateOfProposals(file, bank, progress_keeper=nil)
-		proposals = acquireListOfProposals file, bank
-		if proposals.empty? then raise "Nenhuma proposta localizada" end
-		progress_keeper.total = proposals.length unless progress_keeper.nil?
-		findUfOfEachProposal proposals, progress_keeper
-	end
+  require 'tiny_tds'
+    
+  BANK_TO_SPREADSHEET_COLUMN_NUMBER_OF_PROPOSALS = {
+    "teste" => 'E',
+    "help" => 'E',
+    "ole" => 'H',
+    "itau" => 'I'
+  }
+  
+  def getSpreadSheetColumn filename, bank
+    bank = bank.remover_acentuacao.downcase
+    bank_keys = BANK_TO_SPREADSHEET_COLUMN_NUMBER_OF_PROPOSALS.keys
+    if bank_keys.include? bank
+      return BANK_TO_SPREADSHEET_COLUMN_NUMBER_OF_PROPOSALS[bank]
+    else
+      file_basename = File.basename(filename).remover_acentuacao.downcase
+      bank_keys.each do |k|
+        if file_basename.include? k 
+          return BANK_TO_SPREADSHEET_COLUMN_NUMBER_OF_PROPOSALS[k]
+        end
+      end
+    end
+    raise "O banco desta planilha não pôde ser encontrado"
+  end
+  
+  def acquireListOfProposals file, bank
+    if file.end_with?("xls")
+      require "roo-xls"
+    else
+      require "roo"
+    end
+    
+    spreadsheet =  Roo::Spreadsheet.open(file)
+    column_number = getSpreadSheetColumn file, bank
+    
+    
+    #puts spreadsheet.column(column_number)
+    #raise "teste"
+    spreadsheet.column(column_number).reject {|v| v.nil?}.select {|v| v.is_a?(Integer) or v.is_a?(Float) or v.is_integer?}
+  end
+  
+  def createDatabaseConnection
+    
+    begin
+      TinyTds::Client.new username: user_login, password: user_passwd, host: database_url, database: database_name
+    rescue TinyTds::Error => err
+      raise "Falha ao se conectar ao banco"
+    end 
+  end
+  
+  def getSQL proposal
+    "SELECT UE.SGL_UNIDADE_EMPRESA, UE.SGL_UNIDADE_FEDERACAO, UE.NOM_UNIDADE_EMPRESA, UE.NOM_FANTASIA
+      FROM [CBDATA].[dbo].[PROPOSTA_EMPRESTIMO] AS PE
+        INNER JOIN [CBDATA].[dbo].[UNIDADE_EMPRESA] AS UE ON UE.COD_UNIDADE_EMPRESA = PE.COD_UNIDADE_EMPRESA
+      WHERE PE.NUM_PROPOSTA = '#{proposal}' OR PE.NUM_CONTRATO = '#{proposal}'"
+  end
+  
+  def queryDatabaseForUfOfProposal con, proposal
+    sql = getSQL proposal
+    result = con.execute sql
+    row = result.first
+    if row.nil?
+      nil
+    else
+      uf = row["SGL_UNIDADE_EMPRESA"]
+      uf = uf.nil? ? row["SGL_UNIDADE_FEDERACAO"] : uf
+      uf = uf.nil? ? row["NOM_UNIDADE_EMPRESA"] : uf
+      uf = uf.nil? ? row["NOM_FANTASIA"] : uf
+    end
+  end
+  
+  def findUfOfEachProposal(proposals, progress_keeper=nil)
+    con = createDatabaseConnection
+    raise "Não foi possível acessar o banco de dados" unless con
+    failed_proposals = Array.new
+    response = proposals.collect do |proposal_number|
+      uf = queryDatabaseForUfOfProposal con, proposal_number
+      failed_proposals << proposal_number unless uf
+      progress_keeper.progress += 1 unless progress_keeper.nil?
+      [proposal_number, uf]
+    end
+    con.close
+    return response, failed_proposals
+  end
+  
+  def recoverProposalNumbersAndStateOfProposals(file, bank, progress_keeper=nil)
+    proposals = acquireListOfProposals file, bank
+    if proposals.empty? then raise "Nenhuma proposta localizada" end
+    progress_keeper.total = proposals.length unless progress_keeper.nil?
+    findUfOfEachProposal proposals, progress_keeper
+  end
 end
 
 if __FILE__ == $0 ### script de teste ###
-	include ProcessadorDePendencias
-	
+  include ProcessadorDePendencias
+  
     abort "Passe apenas o caminho do arquivo excel como parametro" unless ARGV.length == 1
-	recoverProposalNumbersAndStateOfProposal(ARGV[0], "teste")
+  recoverProposalNumbersAndStateOfProposal(ARGV[0], "teste")
 end
--- main.rb
+++ main.rb
@@ -11,47 +11,47 @@ class ProcessadorDePendenciasGUI < FXMainWindow
   def initialize(app)
     # Invoke base class initialize first
     super(app, "Processador de pendencias", opts: DECOR_ALL)
-	
+  
     #Create a tooltip
     FXToolTip.new(self.getApp())
-	
+  
     #Controls on top
     controls = FXVerticalFrame.new(self, LAYOUT_SIDE_TOP|LAYOUT_FILL_X,
       padLeft: 40, padRight: 40, padTop: 10, padBottom: 10)
-	
+  
     controlGroup = FXGroupBox.new(controls, "Selecione o banco sendo processado", GROUPBOX_TITLE_CENTER|FRAME_RIDGE|LAYOUT_SIDE_TOP|LAYOUT_FILL_X)
     optionsPopup = FXPopup.new(controlGroup)
-    ["Detecção automática", "BMG", "Bradesco", "Santander", "Itaú", "OLÉ", "HELP"].each { |opt| FXOption.new(optionsPopup, opt) }
-    @bank_select = FXOptionMenu.new controlGroup, optionsPopup, opts: FRAME_RAISED|FRAME_THICK|ICON_BEFORE_TEXT|LAYOUT_FILL_X
+    ["Detecção automática", "Itaú", "OLÉ", "HELP"].each { |opt| FXOption.new(optionsPopup, opt) }
+    @bank_select = FXOptionMenu.new controlGroup, optionsPopup, opts: FRAME_THICK|FRAME_RAISED|ICON_BEFORE_TEXT|LAYOUT_FILL_X
     
     controlGroup = FXGroupBox.new(controls, "Selecione a planilha a ser processada", GROUPBOX_TITLE_CENTER|FRAME_RIDGE|LAYOUT_SIDE_TOP|LAYOUT_FILL_X)
-	progress_keeper = FXProgressBar.new(controlGroup, opts: PROGRESSBAR_NORMAL|PROGRESSBAR_HORIZONTAL|LAYOUT_FILL_X|LAYOUT_SIDE_BOTTOM|PROGRESSBAR_PERCENTAGE)
-    FXButton.new(controlGroup, "Selecionar planilha", opts: LAYOUT_FILL_X|FRAME_THICK|FRAME_RAISED|BUTTON_DEFAULT).connect(SEL_COMMAND) do  
+    progress_keeper = FXProgressBar.new(controlGroup, opts: PROGRESSBAR_NORMAL|PROGRESSBAR_HORIZONTAL|LAYOUT_FILL_X|LAYOUT_SIDE_BOTTOM|PROGRESSBAR_PERCENTAGE)
+    progress_keeper.barColor = FXRGB(0, 150, 0)
+    progress_keeper.textColor = FXRGB(0, 0, 0)
+    selectSpreadsheetBttn = FXButton.new(controlGroup, "Selecionar planilha", opts: LAYOUT_FILL_X|FRAME_THICK|FRAME_RIDGE|BUTTON_DEFAULT)
+    selectSpreadsheetBttn.connect(SEL_COMMAND) do  
       dialog = FXFileDialog.new(self, "Selecione a planilha de pendencias")  
       dialog.selectMode = SELECTFILE_EXISTING
       dialog.patternList = ["Excel Files (*.xls,*.xlsx)"]  
-      
       if dialog.execute != 0
         begin
           processedProposals, failedProposals = recoverProposalNumbersAndStateOfProposals(dialog.filename, @bank_select.current.to_s, progress_keeper)
-		  FXMessageBox::warning self, MBOX_OK, "Algo deu errado...", "As seguintes propostas não puderam ser localizadas:\n" + failedProposals.join("\n")
+          FXMessageBox::warning self, MBOX_OK, "   Algo deu errado...", "As seguintes propostas não puderam ser localizadas:\n\n" + failedProposals.each_slice(4).collect{|s| s.join("       ")}.join("\n") unless failedProposals.empty?
           insertProcessedProposalsToTable processedProposals
-		  progress_keeper.progress = 0
+          progress_keeper.progress = 0
         rescue RuntimeError => err
-          FXMessageBox::error self, MBOX_OK, "Algo deu errado...", err.message
+          FXMessageBox::error self, MBOX_OK, "   Algo deu errado...", err.message
         end
       end  
     end
-    
-    #FXHorizontalSeparator.new(self, LAYOUT_SIDE_TOP|LAYOUT_FILL_X|SEPARATOR_GROOVE)
-	
+  
     FXLabel.new(self, "contato: helpdesk@casebras.com.br", opts: JUSTIFY_RIGHT|LAYOUT_SIDE_BOTTOM)
-	FXHorizontalSeparator.new(self, LAYOUT_SIDE_BOTTOM|LAYOUT_FILL_X|SEPARATOR_GROOVE)
-	
+    FXHorizontalSeparator.new(self, LAYOUT_SIDE_BOTTOM|LAYOUT_FILL_X|SEPARATOR_GROOVE)
+  
     footer = FXVerticalFrame.new(self, LAYOUT_SIDE_BOTTOM|LAYOUT_FILL_X,
       padLeft: 40, padRight: 40, padTop: 10, padBottom: 10)
      
-    FXButton.new(footer, "Copiar para área de transferência", opts: LAYOUT_SIDE_BOTTOM|FRAME_RAISED|FRAME_THICK|LAYOUT_FILL_X).connect(SEL_COMMAND) do 
+    FXButton.new(footer, "Copiar para área de transferência", opts: LAYOUT_SIDE_BOTTOM|FRAME_THICK|FRAME_RIDGE|LAYOUT_FILL_X).connect(SEL_COMMAND) do 
       excelFriendlyContent = String.new
       @table.numRows.times { |r| excelFriendlyContent += "#{@table.getItemText(r,0)}\t#{@table.getItemText(r,1)}\n" }
       Clipboard.copy excelFriendlyContent
@@ -80,14 +80,18 @@ class ProcessadorDePendenciasGUI < FXMainWindow
   end
   
   def insertProcessedProposalsToTable (processedProposals)
+  if processedProposals.empty?
+    @table.setTableSize(1,2)
+  else
     @table.setTableSize(0,2)
+  end
     @table.setColumnText(0, "Propostas")
     @table.setColumnText(1, "UF")
     processedProposals.each do |proposal, uf|
       @table.insertRows(0)
       @table.setItemText(0,0, Integer(proposal).to_s)
       @table.setItemText(0,1, uf.to_s)
-	end
+  end
   end
 end
 
