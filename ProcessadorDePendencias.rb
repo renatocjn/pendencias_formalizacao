1173d63386cb26f8d404d4c687089bd2db508870
Renato Caminha Juacaba Neto <renatocjn@gmail.com>
2018-02-01 17:08:07 -0300

    all spreadsheet columns; more banks
    still need to figure it out the message boxes and the bank ccb query

--- ProcessadorDePendencias.rb
+++ ProcessadorDePendencias.rb
@@ -1,6 +1,6 @@
 class String
   def is_integer?
-    self =~ /^[-+]?[1-9]([0-9]*)?$/
+    self =~ /^[-+]?([0-9]*)?$/
   end
 
   def remover_acentuacao
@@ -16,33 +16,52 @@ module ProcessadorDePendencias
   
   BANK_TO_SPREADSHEET_COLUMN_NUMBER_OF_PROPOSALS = {
    #"bank_name"  => ["proposal_column", "typer_column"],
-    "teste" => ["E", "F"],
-    "help"  => ["E", "J"],
-    "ole"   => ["H", "D"],
-    "itau"  => ["I", "E"],
-    "intermed emprest" => ["A", "N"],
-    "intermed cart" => ["A", "M"],
-    "daycoval" => ["T", "S"],
-    #"centelem" => ["", ""],
-    #"ccb" => ["", ""],
-    "bradesco" => ["A", "J"],
-    "bons" => ["D", "A"],
-    "safra" => ["B", "S"],
-    "sabemi" => ["C", "A"],
-    "pan consignado" => ["A", "Q"],
-    #"banrisul" => ["", ""],
-    "pan cartao" => ["A", "P"]
+    "teste" => "E",
+    "help"  => "E",
+    "ole"   => "H",
+    "itau"  => "I",
+    "intermed emprest" => "A",
+    "intermed cart" => "A",
+    "daycoval" => "T",
+    "cetelem" => "A",
+    #"ccb" => "",
+    "bradesco" => "A",
+    "bons" => "D",
+    "safra" => "B",
+    "sabemi" => "C",
+    "pan consignado" => "A",
+    "banrisul" => "F",
+    "pan cartao" => "A"
   }
+  
+  def openSpreadSheet filename
+    if filename.end_with?("xls")
+      require "roo-xls"
+    else
+      require "roo"
+    end
+    
+    Roo::Spreadsheet.open(filename)
+  end
+  
+  def getSpreadSheetColumnNames filename
+    spreadsheet = openSpreadSheet filename
+    column_names = spreadsheet.row(0)
+    spreadsheet.close
+    return column_names
+  end
   
   def getSpreadSheetColumn filename, bank
     bank = bank.remover_acentuacao.downcase
     bank_keys = BANK_TO_SPREADSHEET_COLUMN_NUMBER_OF_PROPOSALS.keys
     if bank_keys.include? bank
+      puts "Bank: #{bank}"
       return BANK_TO_SPREADSHEET_COLUMN_NUMBER_OF_PROPOSALS[bank]
     else
       file_basename = File.basename(filename).remover_acentuacao.downcase
       bank_keys.each do |k|
         if file_basename.include? k 
+          puts "Bank: #{k}"
           return BANK_TO_SPREADSHEET_COLUMN_NUMBER_OF_PROPOSALS[k]
         end
       end
@@ -50,20 +69,33 @@ module ProcessadorDePendencias
     raise "O banco desta planilha não pôde ser encontrado"
   end
   
-  def acquireListOfProposals file, bank
-    if file.end_with?("xls")
-      require "roo-xls"
-    else
-      require "roo"
-    end
+  def acquireListOfProposalsAndColumns file, bank
+    proposals_column_number = getSpreadSheetColumn file, bank
     
-    spreadsheet =  Roo::Spreadsheet.open(file)
-    proposals_column_number, typer_column_number = getSpreadSheetColumn file, bank
-    
+    spreadsheet = openSpreadSheet file
     proposals = spreadsheet.column(proposals_column_number)
-    typers = spreadsheet.column(typer_column_number)
-    columns = proposals.zip(typers)
-    columns.reject {|p, t| p.nil?}.select {|p, t| p.is_a?(Integer) or p.is_a?(Float) or p.is_integer?}.collect {|p, t| p.is_a?(Float) ? [Integer(p), t] : [p, t]}
+    other_columns = (spreadsheet.first_column..spreadsheet.last_column).each_with_object(Array.new) do |column_idx, column_list|
+      column_list << spreadsheet.column(column_idx) unless column_idx == (proposals_column_number.ord - "A".ord + 1) #converts proposals_column_number to integer index of column"
+    end
+    spreadsheet.close
+    columns = proposals.zip(other_columns.transpose)
+    columns.each_with_object(Array.new) do |(p, t), columns_array|
+      if p.is_a? Integer
+        columns_array << [p.to_s, t]
+      elsif p.is_a? Float
+        columns_array << [p.to_s.slice(0..-3), t] #Removes ".0"
+      elsif p.is_a? String
+        if p =~ /[0-9]{2}\-[0-9]{9}\/[0-9]{2}/ #CETELEM
+          columns_array << [p[3..11], t]
+        elsif p =~ /[0-9]*\-[0-9]/ #Bradesco
+          columns_array << [p[1..-3], t]
+        elsif p.is_integer?
+          columns_array << [p.to_i.to_s, t] #Remove trailing zeroes (BANRISUL)
+        else
+          columns_array << [p.gsub(/(<[^>]*>)|\n|\t/s) {" "}, t]
+        end
+      end
+    end
   end
   
   def createDatabaseConnection
@@ -84,7 +116,6 @@ module ProcessadorDePendencias
   def queryDatabaseUfOfProposal con, proposal
     sql = getSQL proposal
     result = con.execute sql
-    puts result.inspect unless result.is_a? TinyTds::Result
     row = result.first
     result.cancel
     if row.nil?
@@ -92,12 +123,12 @@ module ProcessadorDePendencias
     else
       uf = row["SGL_UNIDADE_EMPRESA"]
       uf = uf.nil? ? row["SGL_UNIDADE_FEDERACAO"] : uf
-      uf = uf.nil? ? row["NOM_UNIDADE_EMPRESA"] : uf
-      uf = uf.nil? ? row["NOM_FANTASIA"] : uf
+      uf = uf.nil? ? row["NOM_UNIDADE_EMPRESA"].split.first : uf
+      uf = uf.nil? ? row["NOM_FANTASIA"].split.first : uf
     end
   end
   
-  def findUfOfEachProposal(proposals_and_typers, progress_keeper=nil, num_connections=15)
+  def findUfOfEachProposal(full_data, progress_keeper=nil, num_connections=15)
     begin
       con_pool = ConnectionPool.new(size: num_connections, timeout: 10*60) { createDatabaseConnection }
       raise "Não foi possível acessar o banco de dados" unless con_pool
@@ -109,25 +140,30 @@ module ProcessadorDePendencias
       end
       
       failed_proposals = Concurrent::Array.new
-      response = Concurrent::Hash.new
+      response = Concurrent::Array.new
       threads = Concurrent::Array.new
       
-      proposals_and_typers.each do |proposal_number, typer|
-        threads << Thread.new do
-          uf = nil
-          con_pool.with do |con|
-            if response.include?(proposal_number) or failed_proposals.include?(proposal_number)
-              Thread.current.terminate
-            else
+      full_data.each do |proposal_number, other_columns|
+        if proposal_number.is_integer?
+          threads << Thread.new do
+            uf = nil
+            con_pool.with do |con|
               uf = queryDatabaseUfOfProposal con, proposal_number
             end
+            
+            if uf
+              other_columns.insert(0, uf)
+              other_columns.insert(0, proposal_number)
+              response << other_columns
+            else
+              failed_proposals << proposal_number unless failed_proposals.include?(proposal_number)
+            end
+            mutex.synchronize {progress_keeper.progress += 1 unless progress_keeper.nil?}
           end
-          
-          if uf
-            response[proposal_number] = [uf, typer]
-          else
-            failed_proposals << proposal_number
-          end
+        else
+          other_columns.insert(0, "Workbank UF")
+          other_columns.insert(0, "Proposta/Contrato")
+          response << other_columns
           mutex.synchronize {progress_keeper.progress += 1 unless progress_keeper.nil?}
         end
       end
@@ -142,20 +178,20 @@ module ProcessadorDePendencias
   end
   
   def recoverProposalNumbersAndStateOfProposals(file, bank, progress_keeper=nil)
-    proposals_and_typers = acquireListOfProposals file, bank
-    if proposals_and_typers.empty?
+    full_data = acquireListOfProposalsAndColumns file, bank
+    if full_data.length == 1
       raise "Nenhuma proposta localizada"
     else
-      puts "Total number of proposals: #{proposals_and_typers.length}"
+      puts "Total number of proposals: #{full_data.length - 1}" #Remove header line
     end
-    progress_keeper.total = proposals_and_typers.length unless progress_keeper.nil?
-    findUfOfEachProposal proposals_and_typers, progress_keeper
+    progress_keeper.total = full_data.length unless progress_keeper.nil?
+    findUfOfEachProposal full_data, progress_keeper
   end
 end
 
 if __FILE__ == $0 ### script de teste ###
   include ProcessadorDePendencias
   
-    abort "Passe apenas o caminho do arquivo excel como parametro" unless ARGV.length == 1
+  abort "Passe apenas o caminho do arquivo excel como parametro" unless ARGV.length == 1
   recoverProposalNumbersAndStateOfProposal(ARGV[0], "teste")
 end
--- main.rbw
+++ main.rbw
@@ -14,7 +14,8 @@ class ProcessadorDePendenciasGUI < FXMainWindow
     # Invoke base class initialize first
     super(app, "Processador de pendencias", opts: DECOR_ALL)
     @logger = Logger.new 'processador_de_pendencias.log', "weekly"
-  
+    @elements = Array.new 
+    
     #Create a tooltip
     FXToolTip.new(self.getApp())
   
@@ -22,44 +23,43 @@ class ProcessadorDePendenciasGUI < FXMainWindow
     controls = FXVerticalFrame.new(self, LAYOUT_SIDE_TOP|LAYOUT_FILL_X,
       padLeft: 40, padRight: 40, padTop: 10, padBottom: 10)
   
-    controlGroup = FXGroupBox.new(controls, "Selecione o banco sendo processado", GROUPBOX_TITLE_CENTER|FRAME_RIDGE|LAYOUT_SIDE_TOP|LAYOUT_FILL_X)
-    optionsPopup = FXPopup.new(controlGroup)
+    @elements << controlGroup = FXGroupBox.new(controls, "Selecione o banco sendo processado", GROUPBOX_TITLE_CENTER|FRAME_RIDGE|LAYOUT_SIDE_TOP|LAYOUT_FILL_X)
+    @elements << optionsPopup = FXPopup.new(controlGroup)
     ["Detecção automática", "Itaú", "OLÉ", "HELP", "INTERMED EMPREST", "INTERMED CART", "Daycoval", "Centelem", "Bradesco", "CCB", "Bons", "Safra", "Sabemi", "PAN Consignado", "PAN Cartão", "Banrisul"].each { |opt| FXOption.new(optionsPopup, opt) }
     @bank_select = FXOptionMenu.new controlGroup, optionsPopup, opts: FRAME_THICK|FRAME_RAISED|ICON_BEFORE_TEXT|LAYOUT_FILL_X
     
-    controlGroup = FXGroupBox.new(controls, "Selecione a planilha a ser processada", GROUPBOX_TITLE_CENTER|FRAME_RIDGE|LAYOUT_SIDE_TOP|LAYOUT_FILL_X)
+    @elements << controlGroup = FXGroupBox.new(controls, "Selecione a planilha a ser processada", GROUPBOX_TITLE_CENTER|FRAME_RIDGE|LAYOUT_SIDE_TOP|LAYOUT_FILL_X)
     @progress_keeper = FXProgressBar.new(controlGroup, opts: PROGRESSBAR_NORMAL|PROGRESSBAR_HORIZONTAL|LAYOUT_FILL_X|LAYOUT_SIDE_BOTTOM|PROGRESSBAR_PERCENTAGE)
     @progress_keeper.barColor = FXRGB(0, 150, 0)
     @progress_keeper.textColor = FXRGB(0, 0, 0)
     @selectSpreadsheetBttn = FXButton.new(controlGroup, "Selecionar planilha", opts: LAYOUT_FILL_X|FRAME_THICK|FRAME_RIDGE|BUTTON_DEFAULT)
     setupSelectSpreadsheetBttn
   
-    FXLabel.new(self, "contato: helpdesk@casebras.com.br", opts: JUSTIFY_RIGHT|LAYOUT_SIDE_BOTTOM)
-    FXHorizontalSeparator.new(self, LAYOUT_SIDE_BOTTOM|LAYOUT_FILL_X|SEPARATOR_GROOVE)
+    @elements << FXLabel.new(self, "contato: helpdesk@casebras.com.br", opts: JUSTIFY_RIGHT|LAYOUT_SIDE_BOTTOM)
+    @elements << FXHorizontalSeparator.new(self, LAYOUT_SIDE_BOTTOM|LAYOUT_FILL_X|SEPARATOR_GROOVE)
   
-    footer = FXVerticalFrame.new(self, LAYOUT_SIDE_BOTTOM|LAYOUT_FILL_X,
-      padLeft: 40, padRight: 40, padTop: 10, padBottom: 10)
+    #footer = FXVerticalFrame.new(self, LAYOUT_SIDE_BOTTOM|LAYOUT_FILL_X,
+    #  padLeft: 40, padRight: 40, padTop: 10, padBottom: 10)
      
-    FXButton.new(footer, "Copiar para área de transferência", opts: LAYOUT_SIDE_BOTTOM|FRAME_THICK|FRAME_RIDGE|LAYOUT_FILL_X).connect(SEL_COMMAND) do 
-      excelFriendlyContent = String.new
-      @table.numRows.times { |r| excelFriendlyContent += "#{@table.getItemText(r,0)}\t#{@table.getItemText(r,1)}\t#{@table.getItemText(r,2)}\n" }
-      Clipboard.copy excelFriendlyContent
-    end
+    #FXButton.new(footer, "Copiar para área de transferência", opts: LAYOUT_SIDE_BOTTOM|FRAME_THICK|FRAME_RIDGE|LAYOUT_FILL_X).connect(SEL_COMMAND) do 
+    #  excelFriendlyContent = String.new
+    #  @table.numRows.times { |r| excelFriendlyContent += "#{@table.getItemText(r,0)}\t#{@table.getItemText(r,1)}\t#{@table.getItemText(r,2)}\n" }
+    #  Clipboard.copy excelFriendlyContent
+    #end
     
     # Contents
-    contents = FXHorizontalFrame.new(self,
+    @elements << contents = FXHorizontalFrame.new(self,
       LAYOUT_CENTER_X|FRAME_NONE|LAYOUT_FILL_X|LAYOUT_FILL_Y, :padding => 10)
      
     @table = FXTable.new(contents, opts: LAYOUT_CENTER_X|LAYOUT_CENTER_Y|LAYOUT_FILL_X|LAYOUT_FILL_Y|TABLE_COL_SIZABLE)
     
     #@table.borderColor = FXRGB(255, 255, 255)
-    @table.visibleRows = 5
+    @table.visibleRows = 10
     @table.visibleColumns = 3
     @table.setTableSize 10, 3
     @table.editable = false
-    @table.setColumnText 0, "Proposta"
-    @table.setColumnText 1, "UF"
-    @table.setColumnText 2, "Digitador"
+    #@table.setColumnText 0, "Proposta"
+    #@table.setColumnText 1, "UF"
   end
   
   def create
@@ -68,20 +68,17 @@ class ProcessadorDePendenciasGUI < FXMainWindow
   end
   
   def insertProcessedProposalsToTable (processedProposals)
-  if processedProposals.empty?
-    @table.setTableSize 1,3 
-  else
-    @table.setTableSize 0,3 
-  end
-    @table.setColumnText 0, "Proposta"
-    @table.setColumnText 1, "UF"
-    @table.setColumnText 2, "Digitador"
-    processedProposals.each do |proposal, uf, typer|
-      @table.insertRows(0)
-      @table.setItemText(0,0, proposal.to_s)
-      @table.setItemText(0,1, uf.to_s)
-      @table.setItemText(0,2, typer.to_s)
-  end
+    if processedProposals.empty?
+      @table.setTableSize 1,3
+    else
+      @table.setTableSize processedProposals.length, processedProposals.collect(&:length).max
+    end
+    
+    processedProposals.each_with_index do |(proposal, uf, *other_values), idx|
+      @table.setItemText(idx,0, proposal.to_s)
+      @table.setItemText(idx,1, uf.to_s)
+      other_values.each_with_index { |v, i| @table.setItemText(idx,2+i, v.to_s) }
+    end
   end
   
   def processSpreadsheet filename
@@ -94,7 +91,7 @@ class ProcessadorDePendenciasGUI < FXMainWindow
       begin
         puts "Starting main process thread"
         processedProposals, failedProposals = recoverProposalNumbersAndStateOfProposals(filename, @bank_select.current.to_s, @progress_keeper)
-        puts "Done processing proposals, proposals found: #{processedProposals.length}, failed proposals: #{failedProposals.length}"
+        puts "Done processing proposals, proposals found: #{processedProposals.length - 1}, failed proposals: #{failedProposals.length}"
         unless failedProposals.empty?
           #failedProposalsMessage = "As seguintes propostas não puderam ser localizadas:\n\n" + failedProposals.each_slice(4).collect{|s| s.join("       ")}.join("\n")
           #window.showWarning failedProposalsMessage
